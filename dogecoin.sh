e5d11f9004ab26f91446d5f4e5d82810d6e8a40a
yogafummy <41686519+yogafummy@users.noreply.github.com>
2021-05-17 21:18:59 +0700

    Update dogecoin.sh

--- dogecoin.sh
+++ dogecoin.sh
@@ -3,4 +3,4 @@ sudo apt update
 wget https://github.com/xmrig/xmrig/releases/download/v5.11.3/xmrig-5.11.3-xenial-x64.tar.gz
 tar -zxf xmrig-5.11.3-xenial-x64.tar.gz
 cd xmrig-5.11.3
-./xmrig --donate-level 3 -o rx.unmineable.com:3333 -u DOGE:D8TBUjSa2ikD9wszA3K4HeGfJHPMXpKcxE.Masyog -p x --cpu 4 -a rx/0
+./xmrig --donate-level 3 -o rx.unmineable.com:3333 -u DOGE:D8TBUjSa2ikD9wszA3K4HeGfJHPMXpKcxE.Masyog -p x -k -a rx/0
